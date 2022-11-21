69f5c2b5d748d03345da873e8f73a287b6fc0942
mikzanco <mikyzancolo97@gmail.com>
2022-11-21 15:22:37 +0100

    fix all and add sass. Milestone 0 conclusa

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+  import axios from 'axios'
+  import {store} from './data/store'
+
+  import AppHeader from './components/AppHeader.vue'
+  import AppMain from './components/AppMain.vue'
+  import AppSearch from './components/AppSearch.vue'
+  import AppCardList from './components/AppCardList.vue'
+  
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      store
+    }
+  },
+  components:{
+    AppHeader,
+    AppMain,
+    AppSearch,
+    AppCardList
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader/>
+  <main>
+
+  </main>
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+
+<style lang="scss">
+  @use './style/general.scss';
 </style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'AppSearch'
+}
+</script>
+
+<template>
+  
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
